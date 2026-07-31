61a8d68566a8b1f2db64987f3035eb2d0475844f
Eduarda Cahu <eduardacahu27@gmail.com>
2026-07-31 00:53:53 -0300

    Criação do projeto Spring Boot

---
+++ src/test/java/br/com/eduarda/studyai/StudyaiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eduarda.studyai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
