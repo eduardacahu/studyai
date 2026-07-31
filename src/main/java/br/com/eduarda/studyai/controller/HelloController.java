7f9fc01e02d1b02ace1cc13b5594b1d3f92ec4bc
Eduarda Cahu <eduardacahu27@gmail.com>
2026-07-31 01:09:13 -0300

    Adiciona endpoint GET /hello

---
+++ src/main/java/br/com/eduarda/studyai/controller/HelloController.java
@@ -0,0 +1,13 @@
+package br.com.eduarda.studyai.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Olá! O StudyAI está funcionando!";
+    }
+}
